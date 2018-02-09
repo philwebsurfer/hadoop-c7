72a6bf8f0452fc8bf2477b14ba52ddf7f9bafe30
philwebsurfer <phil_websurfer@yahoo.com>
2018-02-09 12:56:51 -0600

    Hadoop 2.7.4 on Ubuntu 16.04 Xenial

--- h27u1604/run-first.sh
+++ h27u1604/run-first.sh
@@ -3,8 +3,8 @@ wget -c http://mirror.cogentco.com/pub/apache/hadoop/common/hadoop-2.7.4/hadoop-
 #wget -c http://www-us.apache.org/dist/hadoop/common/hadoop-2.7.4/hadoop-2.7.4.tar.gz
 
 echo 'Execute this to build:'
-echo 'docker build --rm=true -v /sys/fs/cgroup:/sys/fs/cgroup -t local/xenial-hadoop-base .'
+echo 'docker build --rm=true -v /sys/fs/cgroup:/sys/fs/cgroup:ro -t local/xenial-hadoop-base .'
 
 echo 'To run'
-echo 'docker run --rm -p 2222:22  -v /sys/fs/cgroup:/sys/fs/cgroup --rm -it local/xenial-hadoop-base'
+echo 'docker run --rm -p 2222:22  -v /sys/fs/cgroup:/sys/fs/cgroup:ro --rm -it local/xenial-hadoop-base'
 
