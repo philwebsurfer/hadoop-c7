c259113bc3b5e4d034ae92509237cc31041d0836
philwebsurfer <phil_websurfer@yahoo.com>
2018-02-09 19:18:16 -0600

    Hadoop 2.7.4 on Ubuntu 16.04 Xenial

--- h27u1604/run-first.sh
+++ h27u1604/run-first.sh
@@ -6,7 +6,7 @@ echo 'Execute this to build:'
 echo 'docker build --rm=true -v /sys/fs/cgroup:/sys/fs/cgroup:ro -t local/xenial-hadoop-base .'
 
 echo 'To run'
-echo 'docker run --rm -p 2222:22  -v /sys/fs/cgroup:/sys/fs/cgroup:ro --hostname ja006766 --network host --rm -it local/xenial-hadoop-base'
+echo 'docker run --rm -p 2222:22  -v /sys/fs/cgroup:/sys/fs/cgroup:ro --hostname ja175904 --network host --rm -it local/xenial-hadoop-base'
 echo 'docker run --rm -p 2222:22  -v /sys/fs/cgroup:/sys/fs/cgroup:ro --hostname av175875 --network host --rm -it local/xenial-hadoop-base'
 echo 'docker run --rm -p 2222:22  -v /sys/fs/cgroup:/sys/fs/cgroup:ro --hostname em175921 --network host --rm -it local/xenial-hadoop-base'
 
