ef7bb751870825eb8e0be1d509235f688e047ce6
philwebsurfer <phil_websurfer@yahoo.com>
2018-02-23 18:57:33 -0600

    cpudetect.py corrected to correctly detect shares

--- h28u1604/etc/cpudetect.py
+++ h28u1604/etc/cpudetect.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 #@author Jorge III Altamirano Astorga 2018
-#@descr Returns proposed memory in MB for use in Hadoop
-import re 
+#@descr Returns proposed cpus for Hadoop to use
+import re
 import math
 
 cpus = 0
@@ -14,46 +14,31 @@ for line in cpuinfo:
         line = int(line)
         if line > cpus:
             cpus = int(line)
+cpus += 1
 cpuinfo.close()
-print(cpus)
+# print("CPUs system-wide: %d"%cpus)
 
-## procinfo = open('/proc/self/cgroup', 'r')
-## for line in procinfo: 
-##     line = line.strip()
-##     if re.match('.{1,5}:name=systemd:', line):
-##         dockerd = "/sys/fs/cgroup/memory" + \
-##             re.sub("^.{1,5}:name=systemd:", "", line) + \
-##             "/memory.stat"
-##         if not re.match(".*/docker-", dockerd):
-##             continue
-##         #print(dockerd)
-##         memstat = open(dockerd, 'r')
-##         for memline in memstat:
-##             memline = memline.strip()
-##             #https://docs.docker.com/config/containers/runmetrics/#metrics-from-cgroups-memory-cpu-block-io
-##             if re.match("hierarchical_memory_limit", memline):
-##                 memline = re.sub("[^0-9]*", \
-##                     "", memline)
-##                 total = math.floor(int(memline) / 2**20)
-##             #elif re.match("total_rss_huge", memline):
-##             #    memline = re.sub("[^0-9]*", \
-##             #        "", memline)
-##             #    rss_huge = math.floor(int(memline) / 2**20)
-##             elif re.match("total_rss", memline):
-##                 memline = re.sub("[^0-9]*", \
-##                     "", memline)
-##                 rss = math.floor(int(memline) / 2**20)
-##         free = (total - rss)
-##         #print("Free: %d, RSS: %d, Total: %d"%(free, rss, total))
-##         memstat.close()
-## procinfo.close()
-## #print("Total available memory to the container: %d kB"%total)
-## 
-## proposed = math.floor(free*0.8)
-## if (proposed) < (free - 2048):
-##     proposed = free - 2048
-## if proposed < 1536:
-##     proposed = 1536
-## 
-## #print("Proposed: %d"%(proposed))
-## print("%d"%(proposed))
+procinfo = open('/proc/self/cgroup', 'r')
+for line in procinfo:
+    line = line.strip()
+    if re.match('.{1,5}:name=systemd:', line):
+        dockerd = "/sys/fs/cgroup/cpu,cpuacct" + \
+            re.sub("^.{1,5}:name=systemd:", "", line) + \
+            "/cpu.cfs_quota_us"
+        if not re.match(".*/docker-", dockerd):
+            continue
+        # print(dockerd)
+        cpu_quota = open(dockerd, 'r')
+        for cpu_q in cpu_quota:
+            cpu_q = cpu_q.strip()
+            cpus = math.floor(int(cpu_q) / 100000)
+        #print("Free: %d, RSS: %d, Total: %d"%(free, rss, total))
+        cpu_quota.close()
+procinfo.close()
+
+cpus -= 1
+if cpus < 1:  
+    cpus = 1
+# print("Total cpus avail to the container: %d"%cpus)
+print("%d"%(cpus))
+
