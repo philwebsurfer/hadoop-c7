7cb9925986c8a708a525bda891d82950b25b4c0c
philwebsurfer <phil_websurfer@yahoo.com>
2018-02-24 20:10:00 -0600

    fixed script run_first

--- h28u1604-vanilla/run-first.sh
+++ h28u1604-vanilla/run-first.sh
@@ -7,7 +7,7 @@ echo 'docker build --rm=true --force-rm -v /sys/fs/cgroup:/sys/fs/cgroup:ro -t d
 docker build --rm=true --force-rm -v /sys/fs/cgroup:/sys/fs/cgroup:ro -t docker.io/philwebsurfer/xenial-hadoop-vanilla .
 
 echo 'To run'
-echo 'docker run --rm -p 8088:8088 --memory=16G --cpu-period="100000"  --cpu-quota="400000" -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v ~/dfs_n1:/dfs --hostname ja175904 --network host --rm -it philwebsurfer/xenial-hadoop-vanilla'
-echo 'docker run --rm -p 2222:22   --memory=16G --cpu-period="100000"  --cpu-quota="400000" -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v ~/dfs_n1:/dfs --hostname av175875 --network host --rm -it philwebsurfer/xenial-hadoop-vanilla'
-echo 'docker run --rm -p 2222:22   --memory=16G --cpu-period="100000"  --cpu-quota="400000" -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v ~/dfs_n1:/dfs --hostname em175921 --network host --rm -it philwebsurfer/xenial-hadoop-vanilla'
+echo 'docker run --rm --memory=16G --cpu-period="100000"  --cpu-quota="400000" -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v ~/dfs_n1:/dfs --hostname ja175904 --rm -it philwebsurfer/xenial-hadoop-vanilla'
+echo 'docker run --rm --memory=16G --cpu-period="100000"  --cpu-quota="400000" -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v ~/dfs_n1:/dfs --hostname av175875 --rm -it philwebsurfer/xenial-hadoop-vanilla'
+echo 'docker run --rm --memory=16G --cpu-period="100000"  --cpu-quota="400000" -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v ~/dfs_n1:/dfs --hostname em175921 --rm -it philwebsurfer/xenial-hadoop-vanilla'
 
