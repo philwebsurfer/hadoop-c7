e03eb51e49e2b711ddb217d811d1df1a59111df6
philwebsurfer <phil_websurfer@yahoo.com>
2018-02-24 19:38:19 -0600

    now movied downstream

---
+++ h28u1604/etc/bootstrap.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+service ssh start
+cat /etc/hosts.cluster >> /etc/hosts
+if [ -f '/dfs/data/current/VERSION' ]
+then
+        echo HDFS is formatted, skipping formatting
+else 
+        echo HDFS is formatted, formatting...
+        $HADOOP_PREFIX/bin/hdfs namenode -format
+fi
+CPU=$(/etc/cpudetect.py)
+CPUHALF=$(/etc/cpudetecthalf.py)
+MEMORY=$(/etc/memdetect.py)
+sed -i "s/MEMORY/$MEMORY/" /usr/local/hadoop/etc/hadoop/yarn-site.xml.cluster
+sed -i "s/MEMORY/$MEMORY/" /usr/local/hadoop/etc/hadoop/yarn-site.xml.singlenode
+sed -i "s/CPU/$CPU/" /usr/local/hadoop/etc/hadoop/yarn-site.xml.cluster
+sed -i "s/CPU/$CPU/" /usr/local/hadoop/etc/hadoop/yarn-site.xml.singlenode
+sed -i "s/CPU/$CPUHALF/" /usr/local/hadoop/etc/hadoop/yarn-site.xml.cluster
+sed -i "s/CPU/$CPUHALF/" /usr/local/hadoop/etc/hadoop/yarn-site.xml.singlenode
+cp -a /usr/local/hadoop/etc/hadoop/core-site.xml.cluster /usr/local/hadoop/etc/hadoop/core-site.xml
+cp -a /usr/local/hadoop/etc/hadoop/yarn-site.xml.cluster /usr/local/hadoop/etc/hadoop/yarn-site.xml
+cp -a /usr/local/hadoop-2.7.4/etc/hadoop/slaves.cluster  /usr/local/hadoop-2.7.4/etc/hadoop/slaves
+cp -a /usr/local/hadoop-2.7.4/etc/hadoop/mapred-site.xml.cluster /usr/local/hadoop-2.7.4/etc/hadoop/mapred-site.xml
+cp -a /usr/local/hadoop-2.7.4/etc/hadoop/hdfs-site.xml.cluster /usr/local/hadoop-2.7.4/etc/hadoop/hdfs-site.xml
+$HADOOP_PREFIX/sbin/start-dfs.sh
+$HADOOP_PREFIX/sbin/start-yarn.sh
+$HADOOP_PREFIX/mr-jobhistory-daemon.sh start historyserver
+/bin/bash
+
