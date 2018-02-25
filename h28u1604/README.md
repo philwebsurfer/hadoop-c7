5ef1e219155a0537724dc270defed25af079d971
philwebsurfer <phil_websurfer@yahoo.com>
2018-02-24 19:09:14 -0600

    hadoop base

---
+++ h28u1604/README.md
@@ -0,0 +1,2 @@
+# hadoop-h27u16.04
+Hadoop 2.8 in a Docker Container with Ubuntu 16.04 (Xenial)
--- h28u1604/run-first.sh
+++ h28u1604/run-first.sh
@@ -1,12 +1,13 @@
 #!/bin/bash 
-wget -c http://mirror.cogentco.com/pub/apache/hadoop/common/hadoop-2.8.3/hadoop-2.8.3.tar.gz
-#wget -c http://www-us.apache.org/dist/hadoop/common/hadoop-2.8.3/hadoop-2.8.3.tar.gz
+cd hadoop-etc
+tar cfz hadoop-etc.tar.gz * 
 
-echo 'Execute this to build:'
+
+echo 'Executing this to build:'
 echo 'docker build --rm=true --force-rm -v /sys/fs/cgroup:/sys/fs/cgroup:ro -t docker.io/philwebsurfer/xenial-hadoop-base:2.8.3 .'
 
 echo 'To run'
-echo 'docker run --rm -p 8088:8088  -v /sys/fs/cgroup:/sys/fs/cgroup:ro --hostname ja175904 --network host --rm -it local/xenial-hadoop-base'
-echo 'docker run --rm -p 2222:22  -v /sys/fs/cgroup:/sys/fs/cgroup:ro --hostname av175875 --network host --rm -it local/xenial-hadoop-base'
-echo 'docker run --rm -p 2222:22  -v /sys/fs/cgroup:/sys/fs/cgroup:ro --hostname em175921 --network host --rm -it local/xenial-hadoop-base'
+echo 'docker run --rm -p 8088:8088 --memory=16G --cpu-period="100000"  --cpu-quota="400000" -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v ~/dfs_n1:/dfs --hostname ja175904 --network host --rm -it philwebsurfer/xenial-hadoop-base:2.8.3'
+echo 'docker run --rm -p 2222:22   --memory=16G --cpu-period="100000"  --cpu-quota="400000" -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v ~/dfs_n1:/dfs --hostname av175875 --network host --rm -it philwebsurfer/xenial-hadoop-base:2.8.3'
+echo 'docker run --rm -p 2222:22   --memory=16G --cpu-period="100000"  --cpu-quota="400000" -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v ~/dfs_n1:/dfs --hostname em175921 --network host --rm -it philwebsurfer/xenial-hadoop-base:2.8.3'
 
