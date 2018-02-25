9d777a1373171d4553561f49feec6bf15fca0a8d
philwebsurfer <phil_websurfer@yahoo.com>
2018-02-24 19:20:56 -0600

    fixes

--- h28u1604/run-first.sh
+++ h28u1604/run-first.sh
@@ -1,10 +1,11 @@
 #!/bin/bash 
-cd hadoop-etc
-tar cfz hadoop-etc.tar.gz * 
+tar cfz hadoop-etc.tar.gz etc
+cd ..
 
 
 echo 'Executing this to build:'
 echo 'docker build --rm=true --force-rm -v /sys/fs/cgroup:/sys/fs/cgroup:ro -t docker.io/philwebsurfer/xenial-hadoop-base:2.8.3 .'
+docker build --rm=true --force-rm -v /sys/fs/cgroup:/sys/fs/cgroup:ro -t docker.io/philwebsurfer/xenial-hadoop-base:2.8.3 .
 
 echo 'To run'
 echo 'docker run --rm -p 8088:8088 --memory=16G --cpu-period="100000"  --cpu-quota="400000" -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v ~/dfs_n1:/dfs --hostname ja175904 --network host --rm -it philwebsurfer/xenial-hadoop-base:2.8.3'
