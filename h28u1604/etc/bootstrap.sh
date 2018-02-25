e2a2af40a8618117550145d09051a0894fe36e4f
philwebsurfer <phil_websurfer@yahoo.com>
2018-02-24 19:40:37 -0600

    fix bootstrap path

--- h28u1604/etc/bootstrap.sh
+++ h28u1604/etc/bootstrap.sh
@@ -19,11 +19,10 @@ sed -i "s/CPU/$CPUHALF/" /usr/local/hadoop/etc/hadoop/yarn-site.xml.cluster
 sed -i "s/CPU/$CPUHALF/" /usr/local/hadoop/etc/hadoop/yarn-site.xml.singlenode
 cp -a /usr/local/hadoop/etc/hadoop/core-site.xml.cluster /usr/local/hadoop/etc/hadoop/core-site.xml
 cp -a /usr/local/hadoop/etc/hadoop/yarn-site.xml.cluster /usr/local/hadoop/etc/hadoop/yarn-site.xml
-cp -a /usr/local/hadoop-2.7.4/etc/hadoop/slaves.cluster  /usr/local/hadoop-2.7.4/etc/hadoop/slaves
-cp -a /usr/local/hadoop-2.7.4/etc/hadoop/mapred-site.xml.cluster /usr/local/hadoop-2.7.4/etc/hadoop/mapred-site.xml
-cp -a /usr/local/hadoop-2.7.4/etc/hadoop/hdfs-site.xml.cluster /usr/local/hadoop-2.7.4/etc/hadoop/hdfs-site.xml
+cp -a /usr/local/hadoop-2.8.3/etc/hadoop/slaves.cluster  /usr/local/hadoop-2.8.3/etc/hadoop/slaves
+cp -a /usr/local/hadoop-2.8.3/etc/hadoop/mapred-site.xml.cluster /usr/local/hadoop-2.8.3/etc/hadoop/mapred-site.xml
+cp -a /usr/local/hadoop-2.8.3/etc/hadoop/hdfs-site.xml.cluster /usr/local/hadoop-2.8.3/etc/hadoop/hdfs-site.xml
 $HADOOP_PREFIX/sbin/start-dfs.sh
 $HADOOP_PREFIX/sbin/start-yarn.sh
 $HADOOP_PREFIX/mr-jobhistory-daemon.sh start historyserver
 /bin/bash
-
