c379e9b448ba8e4e9b8a0c36b1e377f37bd463b1
philwebsurfer <phil_websurfer@yahoo.com>
2018-02-28 10:09:04 -0600

    adding accumulo, hbase, hive, pig, sqooop, zookeeper

--- h28u1604-vanilla/run-first.sh
+++ h28u1604-vanilla/run-first.sh
@@ -1,5 +1,11 @@
 #!/bin/bash 
 wget -c http://mirror.cogentco.com/pub/apache/hadoop/common/hadoop-2.8.3/hadoop-2.8.3.tar.gz
+wget -c http://mirror.cogentco.com/pub/apache/hive/hive-2.3.2/apache-hive-2.3.2-bin.tar.gz
+wget -c http://mirror.cogentco.com/pub/apache/sqoop/1.4.7/sqoop-1.4.7.bin__hadoop-2.6.0.tar.gz
+wget -c http://mirror.cogentco.com/pub/apache/pig/pig-0.17.0/pig-0.17.0.tar.gz 
+wget -c http://mirror.cogentco.com/pub/apache/accumulo/1.8.1/accumulo-1.8.1-bin.tar.gz
+wget -c http://mirror.cogentco.com/pub/apache/hbase/stable/hbase-1.2.6-bin.tar.gz
+wget -c http://mirror.cogentco.com/pub/apache/zookeeper/zookeeper-3.4.11/zookeeper-3.4.11.tar.gz
 #wget -c http://www-us.apache.org/dist/hadoop/common/hadoop-2.8.3/hadoop-2.8.3.tar.gz
 
 echo 'Executing this to build:'
