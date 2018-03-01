2f0a1a45ae57c200fd0ca2ac7cf8cfa8b6b191f1
philwebsurfer <phil_websurfer@yahoo.com>
2018-03-01 12:42:40 -0600

    ports fix

--- h28u1604/run-first.sh
+++ h28u1604/run-first.sh
@@ -9,10 +9,10 @@ docker build --rm=true --force-rm -v /sys/fs/cgroup:/sys/fs/cgroup:ro -t docker.
 echo 'to run as host network'
 echo 'docker run --rm -p 8088:8088 --memory=16g --cpu-period="100000"  --cpu-quota="400000" -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v ~/dfs_n1:/dfs --hostname ja175904 --network host --rm -it philwebsurfer/xenial-hadoop-base:2.8.3'
 echo 'docker run --rm -p 2222:22   --memory=16g --cpu-period="100000"  --cpu-quota="400000" -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v ~/dfs_n2:/dfs --hostname av175875 --network host --rm -it philwebsurfer/xenial-hadoop-base:2.8.3'
-echo 'docker run --rm -p 2222:22   --memory=16g --cpu-period="100000"  --cpu-quota="400000" -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v ~/dfs_n3:/dfs --hostname em175921 --network host --rm -it philwebsurfer/xenial-hadoop-base:2.8.3'
+echo 'docker run --rm -p 2122:22   --memory=16g --cpu-period="100000"  --cpu-quota="400000" -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v ~/dfs_n3:/dfs --hostname em175921 --network host --rm -it philwebsurfer/xenial-hadoop-base:2.8.3'
 echo
 echo 'to run using docker networking'
 echo 'docker run --rm -p 8088:8088 --memory=16g --cpu-period="100000"  --cpu-quota="400000" -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v ~/dfs_n1:/dfs --hostname ja175904  --rm -it philwebsurfer/xenial-hadoop-base:2.8.3'
 echo 'docker run --rm -p 2222:22   --memory=16g --cpu-period="100000"  --cpu-quota="400000" -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v ~/dfs_n2:/dfs --hostname av175875  --rm -it philwebsurfer/xenial-hadoop-base:2.8.3'
-echo 'docker run --rm -p 2222:22   --memory=16g --cpu-period="100000"  --cpu-quota="400000" -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v ~/dfs_n3:/dfs --hostname em175921  --rm -it philwebsurfer/xenial-hadoop-base:2.8.3'
+echo 'docker run --rm -p 2122:22   --memory=16g --cpu-period="100000"  --cpu-quota="400000" -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v ~/dfs_n3:/dfs --hostname em175921  --rm -it philwebsurfer/xenial-hadoop-base:2.8.3'
 
