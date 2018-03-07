43f02a3a6521f4af7df4fc8da074ec88dcef7319
philwebsurfer <phil_websurfer@yahoo.com>
2018-03-07 13:30:39 -0600

    added latest

--- h28u1604-vanilla/build.sh
+++ h28u1604-vanilla/build.sh
@@ -11,10 +11,10 @@ wget -c http://mirror.cogentco.com/pub/apache/zookeeper/zookeeper-3.4.11/zookeep
 
 echo 'Executing this to build:'
 echo 'docker build --rm=true --force-rm -v /sys/fs/cgroup:/sys/fs/cgroup:ro -t docker.io/philwebsurfer/xenial-hadoop-vanilla:2.8.3 .'
-docker build --rm=true --force-rm -v /sys/fs/cgroup:/sys/fs/cgroup:ro -t docker.io/philwebsurfer/xenial-hadoop-vanilla .
+docker build --rm=true --force-rm -v /sys/fs/cgroup:/sys/fs/cgroup:ro -t docker.io/philwebsurfer/xenial-hadoop-vanilla:2.8.3 .
 
 echo 'To run'
-echo 'docker run --rm --memory=16G --cpu-period="100000"  --cpu-quota="400000" -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v ~/dfs_n1:/dfs --hostname ja175904 --rm -it philwebsurfer/xenial-hadoop-vanilla'
-echo 'docker run --rm --memory=16G --cpu-period="100000"  --cpu-quota="400000" -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v ~/dfs_n1:/dfs --hostname av175875 --rm -it philwebsurfer/xenial-hadoop-vanilla'
-echo 'docker run --rm --memory=16G --cpu-period="100000"  --cpu-quota="400000" -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v ~/dfs_n1:/dfs --hostname em175921 --rm -it philwebsurfer/xenial-hadoop-vanilla'
+echo 'docker run --rm --memory=16G --cpu-period="100000"  --cpu-quota="400000" -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v ~/dfs_n1:/dfs --hostname ja175904 --rm -it philwebsurfer/xenial-hadoop-vanilla:2.8.3'
+echo 'docker run --rm --memory=16G --cpu-period="100000"  --cpu-quota="400000" -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v ~/dfs_n1:/dfs --hostname av175875 --rm -it philwebsurfer/xenial-hadoop-vanilla:2.8.3'
+echo 'docker run --rm --memory=16G --cpu-period="100000"  --cpu-quota="400000" -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v ~/dfs_n1:/dfs --hostname em175921 --rm -it philwebsurfer/xenial-hadoop-vanilla:2.8.3'
 
