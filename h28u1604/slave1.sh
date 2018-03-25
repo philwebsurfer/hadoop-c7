785a3b65a418ca78e5cc48c39503b19dab3a8238
philwebsurfer <phil_websurfer@yahoo.com>
2018-03-24 19:33:05 -0600

    labelling update

--- h28u1604/slave1.sh
+++ h28u1604/slave1.sh
@@ -5,6 +5,7 @@ docker stop $master 2>/dev/null || \
 docker rm $master 2>/dev/null
 docker run -d \
   -l slave1 \
+  -n slave1 \
   --memory=20g \
   --cpu-period="100000"  --cpu-quota="400000" \
   -v /sys/fs/cgroup:/sys/fs/cgroup:ro \
